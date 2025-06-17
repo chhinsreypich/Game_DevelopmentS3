f907f53314f37c04fdd1c2f0b7d59b80bd8f6b61
chhinsreypich <chhinsreypich777@gmail.com>
2025-06-17 09:10:26 +0700

    FirstController fixed

--- src/main/java/com/example/demo/FirstController.java
+++ src/main/java/com/example/demo/FirstController.java
@@ -13,7 +13,6 @@ public class FirstController {
 
     ///  if i want to make a change the in code
     ///  then  ctrl + k and comment "filename fix " and then start push it like first commit
-    ///
     @GetMapping("/")
     public String first(){
        return "home";
