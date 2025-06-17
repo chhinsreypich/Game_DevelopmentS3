d10c0c60ee8b18fe68ee25d1f8447358768ddba8
chhinsreypich <chhinsreypich777@gmail.com>
2025-06-17 09:09:03 +0700

    FirstController fix

--- src/main/java/com/example/demo/FirstController.java
+++ src/main/java/com/example/demo/FirstController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class FirstController {
 
+    ///  if i want to make a change the in code
+    ///  then  ctrl + k and comment "filename fix " and then start push it like first commit
     @GetMapping("/")
     public String first(){
        return "home";
